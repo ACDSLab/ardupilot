abbc2a731eb59fb4a3a1bd95bdf961a18fd89a88
leewhitcher <leewhitcher@gmail.com>
2019-04-10 22:38:11 -0400

    LM_max lookup and DI restructure
    incorporate LM_max lookup table using polyfit from 6DOF load cell data, and restructure DI to be hitting gyroscopic modes only

--- libraries/CEMAV/Dynamic_Inversion.cpp
+++ libraries/CEMAV/Dynamic_Inversion.cpp
@@ -29,8 +29,14 @@ const AP_Param::GroupInfo Dynamic_Inversion::var_info[] = {
 };
 
 void Dynamic_Inversion::compute_DI_pq(float cur_p, float cur_q, float cur_omega, float p_dot_c, float q_dot_c, float (&commands)[2]) {
-    commands[0] = (_Kpq*cur_p + _Ir*cur_omega*cur_q + _Ixy*p_dot_c) / _LM_max;
-    commands[1] = (_Kpq*cur_q - _Ir*cur_omega*cur_p + _Ixy*q_dot_c) / _LM_max;
+    // commands[0] = (_Kpq*cur_p + _Ir*cur_omega*cur_q + _Ixy*p_dot_c) / _LM_max;
+    // commands[1] = (_Kpq*cur_q - _Ir*cur_omega*cur_p + _Ixy*q_dot_c) / _LM_max;
+	
+	// Fit of pitch/roll control moment at max flap deflection to motor speed (rad/s, assumes constant 17 degree fishtail trim)
+	// Max moment in absolute terms is 3.54Nm@6000rpm, 4.94Nm@7000rpm, 6.57Nm@8000rpm
+	LM_max = 0.00001052*cur_omega*cur_omega - 0.0009282*cur_omega; 
+	commands[0] = _Ir*cur_omega*cur_q / LM_max + p_dot_c;
+    commands[1] = -_Ir*cur_omega*cur_p / LM_max + q_dot_c;
 }
 
 
